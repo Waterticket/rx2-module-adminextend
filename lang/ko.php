767bc960f460d8c8698eeeb77a51cc1231041fa5
Waterticket <matthew218@naver.com>
2022-09-15 14:44:23 +0000

    initial commit

---
+++ lang/ko.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang->cmd_adminextend = '관리자 확장팩';
+$lang->cmd_adminextend_general_config = '기본 설정';
+$lang->cmd_adminextend_example_config = '예제 설정';
---
+++ models/something.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Rhymix\Modules\Adminextend\Models;
+
+use Rhymix\Modules\Adminextend\Base;
+use Rhymix\Framework\Cache;
+use Rhymix\Framework\DB;
+use Rhymix\Framework\Exception;
+use Rhymix\Framework\Storage;
+use BaseObject;
+use Context;
+
+/**
+ * 관리자 확장팩
+ * 
+ * Copyright (c) Waterticket
+ * 
+ * Generated with https://www.poesis.org/tools/modulegen/
+ */
+class Something
+{
+	
+}
